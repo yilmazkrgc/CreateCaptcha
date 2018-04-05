733aabd4fb786ce8d9bc7aef93b5cbe19e42dbf4
karaagacylmaz <37292831+karaagacylmaz@users.noreply.github.com>
2018-04-05 16:54:02 +0300

    Add files via upload

---
+++ CreateCaptcha/CreateCaptcha/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace CreateCaptcha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.pctr = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pctr)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(51, 41);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(207, 29);
+            this.btnCreate.TabIndex = 0;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // pctr
+            // 
+            this.pctr.Location = new System.Drawing.Point(51, 105);
+            this.pctr.Name = "pctr";
+            this.pctr.Size = new System.Drawing.Size(207, 142);
+            this.pctr.TabIndex = 1;
+            this.pctr.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(316, 317);
+            this.Controls.Add(this.pctr);
+            this.Controls.Add(this.btnCreate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pctr)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.PictureBox pctr;
+    }
+}
+
---
+++ CreateCaptcha/CreateCaptcha/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CreateCaptcha
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //captcha için random string üretir
+        //generates random text for captcha
+        string GetRandomTextForCaptcha()
+        {
+            StringBuilder sb = new StringBuilder();
+            string alphabet = "012345679ABCDEFGHIJKLMNOPRSTUVWXZabcdefghijkhlmnopqrstuvwxyz";
+            Random rnd = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                sb.Append(alphabet[rnd.Next(alphabet.Length)]);
+            }
+
+            return sb.ToString();
+        }
+
+        //captcha için Bitmap oluşturup Binary serialize işlemini gerçekleştirir.
+        //genereates Bitmap and perform Binary Serialize for Captcha
+        public byte[] ConvertImageSerializeForCaptcha()
+        {
+
+            //Fontların belirlenmesi
+            string[] fontNames = new string[]
+   {
+ "Comic Sans MS",
+ "Arial",
+ "Times New Roman",
+ "Georgia",
+ "Verdana",
+ "Geneva"
+   };
+
+            //Captcha için metin oluşturma
+            string randomText = GetRandomTextForCaptcha();
+
+
+            Graphics draw;
+            Bitmap img;
+            Random random = new Random();
+
+            img = new Bitmap(180, 80);
+            draw = Graphics.FromImage(img);
+
+            //Rastgele font belirleme(farklı boyutlarda)
+            Font font = new Font(fontNames[random.Next(6)], random.Next(20, 40));
+
+            //arkaplan ve yazı rengi rastgele belirlenir.
+            Color backColor = Color.FromArgb(random.Next(128), random.Next(128), random.Next(128));
+            Color textColor = Color.FromArgb(random.Next(128, 256), random.Next(128, 256), random.Next(128, 256));
+
+            //Arka planın çizgili olması için gerekli fırça tipi seçilir.
+            Brush backBrush = new HatchBrush(HatchStyle.DashedVertical, backColor);
+
+            //Yazı tipinin normal olması için gerekli fırça tipi seçilir.
+            Brush textBrush = new SolidBrush(textColor);
+
+            //arkaplan oluşturulur.
+            draw.FillRectangle(backBrush, 0, 0, img.Size.Width, img.Size.Height);
+
+            //captcha yazısı oluşturulur.
+            draw.DrawString(randomText, font, textBrush, 0, 0);
+
+            draw.Save();
+
+            font.Dispose();
+            backBrush.Dispose();
+            textBrush.Dispose();
+            draw.Dispose();
+
+            var stream = new MemoryStream();
+            var formatter = new BinaryFormatter();
+
+            //image nesnesi serialize edilir.
+            formatter.Serialize(stream, img);
+
+            byte[] temp = stream.ToArray();
+
+            stream.Dispose();
+
+            return temp;
+
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            //Binary deserialize işlemi yapılır ve dönen object tipi Image tipine cast edilir.
+            byte[] captcha = ConvertImageSerializeForCaptcha();
+            var stream = new MemoryStream(captcha);
+            var formatter = new BinaryFormatter();
+            Image img = (Image)formatter.Deserialize(stream);
+            pctr.Image = img;
+        }
+    }
+}
